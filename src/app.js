0d7ee9568bdab1fd0a5e82198e933e33c8f9e735
Sajku <kielek0412@gmail.com>
2022-07-28 11:16:21 +0200

    Add files from another project.

---
+++ src/app.js
@@ -0,0 +1,30 @@
+const prevBtn = document.querySelector("#formLinkLeft");
+const nextBtn = document.querySelector("#formLinkRight");
+const allDescriptions = document.querySelectorAll(".description");
+const mainImg = document.querySelector("#photo");
+
+prevBtn.addEventListener("click", function() {
+    var temp = mainImg.src;
+    var result = temp.split("/")[4].split(".")[0];
+    changeImg(Number(result) - Number(1));
+});
+
+nextBtn.addEventListener("click", function() {
+    var temp = mainImg.src;
+    var result = temp.split("/")[4].split(".")[0];
+    changeImg(Number(result) + Number(1));
+});
+
+function changeImg(x) {
+
+    if (x <= 0) {
+        x = 54;
+    } else if (x >= 55) {
+        x = 1;
+    }
+    mainImg.src = "/gallery/photos/" + x + ".jpg";
+    allDescriptions.forEach(function(e) {
+        e.classList.remove("visible");
+    });
+    document.getElementById(x).classList.add("visible");
+}
